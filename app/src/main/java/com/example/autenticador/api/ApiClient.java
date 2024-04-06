21e51513079721d6bdddbb6d445742805c8b3892
Gerardo Plata <gerardogplatau@gmail.com>
2024-04-06 15:07:29 -0600

    se modifico la ip

--- app/src/main/java/com/example/autenticador/api/ApiClient.java
+++ app/src/main/java/com/example/autenticador/api/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     public static Retrofit getInstance(){
         return new Retrofit.Builder()
-                .baseUrl("http://192.168.100.2:8000/api/")
+                .baseUrl("http://10.0.1.1:55555/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
